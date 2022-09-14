f7c0bd564b6118240a1dd936bf5060fe63cd12d8
Daniil <sidorchuk.daniil@gmail.com>
2022-09-14 22:08:14 +0300

    Commit 9. Created section 'feedback' and made responsive markup for section 'catalog' and 'feedback'

--- src/js/script.js
+++ src/js/script.js
@@ -1,33 +1,11 @@
 
-// $(document).ready(function(){
-//     $('.carousel__inner').slick({
-//         speed: 1200,
-//         prevArrow: '<button type="button" class="slick-prev"><img src="icons/carousel/left.svg"</button>',
-//         nextArrow: '<button type="button" class="slick-next"><img src="icons/carousel/right.svg"</button>',
-//         responsive: [
-//             {
-//                 breakpoint: 768,
-//                 settings: {
-//                   dots: true,
-//                   arrows: false
-//                 }
-//             }
-//         ]
-//     });
-//   });
-
 const slider = tns({
     container: '.carousel__inner',
     items: 1,
     slideBy: 'page',
     autoplay: false,
     nav: false,
-    controls: false,
-    responsive: {
-        767: {
-            viewportMax: 100
-        }
-    }
+    controls: false
 });
 
 document.querySelector('.prev').addEventListener('click', () => {
